5d43d53f4d85da67f4e6b3c961b38e4c0fec87a5
PatrykBudasz <budasz1997@gmail.com>
2020-01-09 18:06:12 +0100

    Add new files

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace kolko_krzyzyk
+{
+    partial class kolko
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowaGraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pomocToolStripMenuItem,
+            this.pomocToolStripMenuItem1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(350, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowaGraToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.pomocToolStripMenuItem.Text = "Plik";
+            // 
+            // nowaGraToolStripMenuItem
+            // 
+            this.nowaGraToolStripMenuItem.Name = "nowaGraToolStripMenuItem";
+            this.nowaGraToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.nowaGraToolStripMenuItem.Text = "Nowa gra";
+            this.nowaGraToolStripMenuItem.Click += new System.EventHandler(this.nowaGraToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pomocToolStripMenuItem1
+            // 
+            this.pomocToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.pomocToolStripMenuItem1.Name = "pomocToolStripMenuItem1";
+            this.pomocToolStripMenuItem1.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem1.Text = "Pomoc";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(95, 22);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(12, 27);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(104, 104);
+            this.A1.TabIndex = 2;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(122, 27);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(104, 104);
+            this.A2.TabIndex = 3;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(232, 27);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(104, 104);
+            this.A3.TabIndex = 4;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(232, 137);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(104, 104);
+            this.B3.TabIndex = 7;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(122, 137);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(104, 104);
+            this.B2.TabIndex = 6;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(12, 137);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(104, 104);
+            this.B1.TabIndex = 5;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(232, 247);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(104, 104);
+            this.C3.TabIndex = 10;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(122, 247);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(104, 104);
+            this.C2.TabIndex = 9;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(12, 247);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(104, 104);
+            this.C1.TabIndex = 8;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.klikniecie_przycisku);
+            // 
+            // kolko
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 367);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip2);
+            this.MaximizeBox = false;
+            this.Name = "kolko";
+            this.ShowIcon = false;
+            this.Text = "Kółko i krzyżyk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem1;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.ToolStripMenuItem nowaGraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kolko_krzyzyk
+{
+    public partial class kolko : Form
+    {
+        //TURA
+        bool turn = true;
+
+        //POCZĄTKOWA LICZBA KLIKNIĘĆ W PRZYCISK JEŚLI KLIKNIEMY WSZYSTKIE 9 W PÓŹNIEJSZYM ETAPIE ZOSTAJE POKAZANA WIADOMOŚĆ O TYM ŻE NIKT NIE WYGRAŁ
+        int liczenie = 0;
+        public kolko()
+        {
+            InitializeComponent();
+        }
+
+        //GRA ROZPOCZYNA SIĘ KOMUNIKATEM KOGO KOLEJ
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tura gracza X", "Tura");
+        }
+
+     
+        //OBSŁUGA W MENU OPCJI INFO
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Stworzone przez: Patryk Budasz, Mikołaj Bosiacki", "Informacje");
+        }
+
+        //OBSŁUGA W MENU OPCJI EXIT
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //OBSŁUGA W MENU OPCJI NOWA GRA
+        private void nowaGraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+      
+
+        private void klikniecie_przycisku(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            //WIADOMOŚĆ W MESSAGEBOXIE KOGO KOLEJ
+            if (turn == true)
+                MessageBox.Show("Tura gracza O", "Tura");
+            else
+                MessageBox.Show("Tura gracza X", "Tura");
+
+            //ZMIANA GRACZA
+            if (turn)       
+                b.Text = "x";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            liczenie++;
+
+            sprawdzanieZwyciezcy();
+        }
+
+        private void sprawdzanieZwyciezcy()
+        {
+            bool toZwyciezca = false;
+            //WARIANTY KIEDY MOŻNA WYGRAĆ
+            //POZIOMO
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                toZwyciezca = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                toZwyciezca = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                toZwyciezca = true;
+
+            //PIONOWO
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                toZwyciezca = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                toZwyciezca = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                toZwyciezca = true;
+
+            //NA SKOS
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                toZwyciezca = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+                toZwyciezca = true;
+            
+            //SPRAWDZENIE KTÓRY GRACZ ZWYCIĘŻYŁ I CZY W OGÓLE KTOŚ WYGRAŁ
+            if (toZwyciezca)
+            {
+                wylaczeniePrzyciskow();
+
+                string zwyciezca = "";
+                if (turn)
+                    zwyciezca = "O";
+                else
+                    zwyciezca = "X";
+
+                MessageBox.Show(zwyciezca + " wygrywa!", "Brawo");
+            }
+            else
+            {
+                if(liczenie == 9)
+                    MessageBox.Show("Koniec gry, brak zwycięzcy :(", "Koniec");
+            }
+
+
+            //PO SKOŃCZENIU GRY RESZTA PRZYCISKÓW STAJE SIĘ NIEDOSTĘPNA
+             void wylaczeniePrzyciskow()
+            {
+                try
+                {
+                    foreach (Control c in Controls)
+                    {
+                        Button b = (Button)c;
+                        b.Enabled = false;
+
+                    }
+                }
+                catch { }
+            }
+        }
+
+    }
+}
